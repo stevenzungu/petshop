7bf5881ca8f819a29f11b6251bbf28ac9bd42952
Mac <Mac@Macs-Air.itpro.tv>
2017-03-10 19:27:55 -0500

    successfully implemented the delete route.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
 
+//powerup -- middleware
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+
 //connect to the database
 mongoose.connect(mongoURI);
-//Create the mongoose Scema
+//Create the mongoose Schema
 var animalSchema = mongoose.Schema({
   color: String,
   size: String,
@@ -61,9 +67,39 @@ server.get('/animals/:id', function(req, res){
     }
   });
 });
+
+
+
 //POST /animals
+server.post('/animals', function(req, res){
+  var animal = new Animal(req.body);
+  animal.save(function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    }  else{
+      res.status(201).json({
+        msg: "Success"
+      });
+      }
+    });
+});
 //PUT /animals/:id
 //DELETE /animals/:id
+server.delete('/animals/:id', function(req, res){
+  Animal.remove({_id: req.params.id}, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(200).json({
+        msg: "Successfully delete"
+      });
+    }
+  });
+});
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
